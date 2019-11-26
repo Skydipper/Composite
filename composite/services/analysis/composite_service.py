fbafff06708604dff0f0a3159f3edcf1238f5499
Benjamin A. Laken <benlaken@gmail.com>
2019-11-26 18:10:28 +0100

    updated code

--- composite/services/analysis/composite_service.py
+++ composite/services/analysis/composite_service.py
@@ -15,55 +15,46 @@ class CompositeService(object):
                         band_viz, get_dem, cloudscore_thresh):
         """blah"""
         logging.info(f"[COMPOSITE SERVICE]: Creating composite")
+        logging.info(f"[COMPOSITE SERVICE] EE LIB {ee.__version__}")
+        result_dic = {}
         try:
             features = geojson.get('features')
- #           region = [ee.Geometry(feature['geometry']) for feature in features][0]
-            region = ee.Geometry({
-                'type': 'Polygon',
-                'coordinates': [get_clip_vertex_list(geojson)]
-            })
-            logging.info(f'\n[COMPOSITE SERVICE] DATE RANGE 1: {date_range}')
+            region = [ee.Geometry(feature['geometry']) for feature in features][0]
+            clip_region = ee.Geometry({'type': 'Polygon',
+                                       'coordinates': [get_clip_vertex_list(geojson)]
+                                   })
             if not date_range:
-                date_range = CompositeService.get_last_3months()
-            else:
-                date_range = date_range[1:-1].split(',')
-            logging.info(f'\n[COMPOSITE SERVICE] DATE RANGE 2: {date_range}')
-            sat_img = CompositeService.get_sat_img(instrument, region, date_range, cloudscore_thresh)
+                dates = CompositeService.get_last_3months()
+            dates = date_range[1:-1].split(',')
+            logging.info(f"[COMPOSITE SERVICE] INSTRUMENT {instrument}")
+            logging.info(f"[COMPOSITE SERVICE] Dates {dates}")
+            if instrument.lower() == 'landsat':
+                sat_img = ee.ImageCollection("LANDSAT/LC08/C01/T1_TOA").filter(ee.Filter.lte('CLOUD_COVER', cloudscore_thresh))
+                sat_img = sat_img.filterDate(dates[0].strip(), dates[1].strip()).median()
+                logging.info(f"[COMPOSITE SERVICE]: Sat image 1")
+            elif instrument.lower() == 'sentinel':
+                sat_img = ee.ImageCollection('COPERNICUS/S2').filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', cloudscore_thresh))
+                sat_img = sat_img.filterBounds(region).filterDate(dates[0].strip(), dates[1].strip()).median()
+                sat_img = sat_img.divide(100*100)
+                logging.info(f"[COMPOSITE SERVICE]: Sat image 2")
+            logging.info(f"[COMPOSITE SERVICE]: Sat image 3")
             image = sat_img.visualize(**band_viz)
-            tmp_thumb = image.getThumbUrl({'dimensions': thumb_size})
-            tmp_tile = CompositeService.get_image_url(image)
-            result_dic = {'thumb_url': tmp_thumb,
-                            'tile_url': tmp_tile}
-            logging.error(f'[Composite Service]: result_dic {result_dic}')
+            result_dic['thumb_url'] = image.getThumbUrl({'dimensions': thumb_size, 'region': region})
+            result_dic['tile_url'] = CompositeService.get_image_url(image.clip(clip_region))
             if get_dem:
-                dem_img = ee.Image('JAXA/ALOS/AW3D30_V1_1').select('AVE').clip(region)
-                #getThumbUrl({'region':region, 'dimensions':thumb_size,\
-                #        'min':-479, 'max':8859.0})
-                dem_url = dem_img.getThumbUrl({'dimensions':thumb_size, 'min':-479, 'max':8859.0})
+                dem_img = ee.Image('JAXA/ALOS/AW3D30_V1_1').select('AVE')
+                dem_url = dem_img.getThumbUrl({'dimensions':thumb_size, 'min':-479, 'max':8859.0, 'region': region})
                 result_dic['dem'] = dem_url
             return result_dic
         except Exception as error:
             logging.error(str(error))
-            raise CompositeError(message='Error in composite imaging')
+            raise CompositeError(message=f'Error in composite imaging {error}')
 
     @staticmethod
     def get_last_3months():
         date_weeks_ago = datetime.now() - timedelta(weeks=21)
         date_weeks_ago = date_weeks_ago.strftime("%Y-%m-%d")
         return [date_weeks_ago, datetime.today().strftime('%Y-%m-%d')]
-
-
-    @staticmethod
-    def get_sat_img(instrument, region, date_range, cloudscore_thresh):
-        if(instrument == 'landsat'):
-            sat_img = ee.ImageCollection("LANDSAT/LC08/C01/T1_TOA").filter(ee.Filter.lte('CLOUD_COVER', cloudscore_thresh))
-        else:
-            sat_img = ee.ImageCollection('COPERNICUS/S2').filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', cloudscore_thresh))
-        sat_img = sat_img.filterBounds(region).filterDate(date_range[0].strip(), date_range[1].strip())\
-                    .median().clip(region)
-        if(instrument == 'sentinel'):
-            sat_img = sat_img.divide(100*100)
-        return sat_img
 
     @staticmethod
     def get_image_url(source):
