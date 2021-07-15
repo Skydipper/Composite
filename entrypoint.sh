0fb63815e98571cc4286e9b2f1abdfccfa88e0c8
andrea rota <andrea.rota@vizzuality.com>
2021-07-15 13:31:05 +0100

    (wip) update earthengine-api and auth flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,14 @@ set -e
 case "$1" in
     develop)
         echo "Running Development Server"
-        echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.json
         exec python main.py
         ;;
     test)
         echo "Running tests"
-        echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.json
         exec pytest --cov=composite composite/tests/
         ;;
     start)
         echo "Running Start"
-        echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.json
         exec gunicorn -c gunicorn.py composite:app
         ;;
     *)
